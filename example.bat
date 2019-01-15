3fd2a878ae76d0360c68416a5884f4d7f47498ae
EhssanD <dalvand.ehsan@gmail.com>
2019-01-15 04:17:51 +0330

    updated example

--- example.bat
+++ example.bat
@@ -6,12 +6,13 @@ set GPU_MAX_HEAP_SIZE=100
 
 :: Replace the -o, -u, and -p arguments with your own pool, user, and password.
 :: All three arguments are required.
-sgminer.exe -k x16r -o stratum+tcp://minepool.com:3636 -u RQfsnqLb4ApUcQYMJG3DxiHJDCtd6HhB3F -p x -X 320 --gpu-platform=1 --device 1 -g 2
+sgminer.exe -k argon2d -o stratum+tcp://dyn.noncer.pro:4239 -u DKPnTs1s71DtesAvvLMchtsj4gRFxphW55 -p x -g 2
 pause
 
 :: Notes:
-:: - xIntensities of 128-1024 usually work fine. Feel free to tweak it to your liking, but
-::   keep the xIntensity to a multiple of 64.
+:: - Use intensity (-I) combined with threads (-g) to utilize more GPU memory.
+::   notice that this value is per thread and regardless of your GPU, you can not allocate more than 4 GB of memory per thread.
+::   Increase the number of threads instead to fully utilize the available GPU memory.
 ::   Note that higher intensities can sometimes lower your hashrate, since it creates more
 ::   work units at once; after a certain point, the work units take longer to return and
 ::   this will actually decrease your hashrate.
